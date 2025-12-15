248cd3ce2938c3313c9e27d1bf269cef28531615
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-12-15 19:56:33 +0530

    `Added eslint disable for react-hooks/exhaustive-deps in QRScannerModal component`

--- app/(main)/my-events/_components/qr-scanner-modal.jsx
+++ app/(main)/my-events/_components/qr-scanner-modal.jsx
@@ -115,6 +115,7 @@ export default function QRScannerModal({ isOpen, onClose }) {
       }
       setScannerReady(false);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isOpen]);
 
   return (
